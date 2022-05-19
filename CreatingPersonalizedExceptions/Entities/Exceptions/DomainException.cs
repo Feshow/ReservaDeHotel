21505abb01ef19a578f36b234c2d5c8a7a9a7927
Delesporte DES <delesporte@mgcontecnica.com.br>
2022-05-19 16:25:47 -0300

    Fist commit

---
+++ CreatingPersonalizedExceptions/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+// É uma exceção de dominio
+namespace CreatingPersonalizedExceptions.Entities.Exceptions
+{
+    // É uma subclasse de ApplicationException
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
